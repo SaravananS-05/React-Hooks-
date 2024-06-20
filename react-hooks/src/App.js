6d091e68711aa03008a06c59b039302cd5a8ab67
Saravanan <saravanans.22cse@kongu.edu>
2024-06-20 21:04:01 +0530

    first commit

---
+++ react-hooks/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>useState Example</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-hooks/src/Counter.js
@@ -0,0 +1,22 @@
+// src/Counter.js
+import React, { useState, useEffect } from 'react';
+import './Counter.css';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  }, [count]);
+
+  return (
+    <div className="counter">
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
